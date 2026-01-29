16ce826f0ca0769e8ae32f09f785f1bb23143ea2
obviouslyweb <conbutter12@gmail.com>
2026-01-28 17:18:54 -0700

    Added faculty controller.

---
+++ src/controllers/faculty/faculty.js
@@ -0,0 +1,33 @@
+import { getFacultyById, getSortedFaculty } from '../../models/faculty/faculty.js';
+
+// Route handler for the faculty list
+const facultyListPage = (req, res) => {
+    const sortBy = req.query.sort;
+    const faculty = getSortedFaculty(sortBy);
+
+    res.render('faculty', {
+        title: 'Faculty List',
+        faculty: faculty
+    });
+};
+
+// Route handler for looking up individual faculty
+const facultyDetailPage = (req, res, next) => {
+    const facultyId = req.params.facultyId;
+    const faculty = getFacultyById(facultyId);
+
+    // If faculty doesn't exist, create 404 error
+    if (!faculty) {
+        const err = new Error(`Faculty ${facultyId} not found`);
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render('faculty-detail', {
+        title: `${faculty.name} - ${faculty.title}`,
+        faculty,
+        facultyId
+    });
+};
+
+export { facultyListPage, facultyDetailPage };
