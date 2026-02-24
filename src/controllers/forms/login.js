843f4abba838b0a964bfb3ab4f6af0cfcdde01ec
obviouslyweb <conbutter12@gmail.com>
2026-02-24 16:43:26 -0700

    Implemented advanced form validation.

--- src/controllers/forms/login.js
+++ src/controllers/forms/login.js
@@ -12,11 +12,14 @@ const loginValidation = [
         .trim()
         .isEmail()
         .withMessage('Please provide a valid email address')
-        .normalizeEmail(),
-
+        .normalizeEmail()
+        .isLength({ max: 255 })
+        .withMessage('Email address is too long; please use a shorter email address.'),
     body('password')
-        .isLength({ min: 8 })
-        .withMessage('Password is required')
+        .notEmpty()
+        .withMessage('Password must not be empty')
+        .isLength({ min: 8, max: 128 })
+        .withMessage('Password must be between 8 and 128 characters')
 ];
 
 /**
